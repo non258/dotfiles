72b9715fbc6cd42c8011d7877d6fd90317fb572c
non258 <c0117037bf@edu.teu.ac.jp>
2018-03-05 18:01:36 +0900

    fix linc.sh

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.config ~/.config
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.bash_profiles ~/.bash_profiles
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
