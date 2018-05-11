783240fd08cae8dbe63a62293ad202188e3f4209
non258 <c0117037bf@edu.teu.ac.jp>
2018-05-11 10:52:59 +0900

    del

--- .config/google-chrome/Default/Extensions/ophjlpahpchlmihnnnihgmmeilfjmjjc/2.1.4_0/src/jDjNT.js
+++
@@ -1,1 +0,0 @@
-importScripts("uVLFd.js");importScripts("ivKTh.js");
--- link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.config ~/.config
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
