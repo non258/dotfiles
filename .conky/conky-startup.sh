f99271a213715edd2894fdc5bdecd66661321c3e
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-26 14:28:42 +0900

    conky

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -5,6 +5,6 @@ conky -c "/home/nozomi/.conky/Conky Seamod/conky_seamod" &
 cd "/home/nozomi/.conky/Gotham"
 conky -c "/home/nozomi/.conky/Gotham/Gotham" &
 cd "/home/nozomi/.conky/TeejeeTech"
-conky -c "/home/nozomi/.conky/TeejeeTech/CPU Panel (4-core)" &
+conky -c "/home/nozomi/.conky/TeejeeTech/CPU Panel (8-core)" &
 cd "/home/nozomi/.conky/TeejeeTech"
 conky -c "/home/nozomi/.conky/TeejeeTech/Process Panel" &
