7737de5974e2034c16a83eb0a3ecdbf50e440b75
non258 <c0117037bf@edu.teu.ac.jp>
2018-10-10 10:24:34 +0900

    link.sh

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ rm ~/.bashrc
 rm -r ~/.config
 
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zplug ~/.zplug
 ln -sf ~/dotfiles/.config ~/.config
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
