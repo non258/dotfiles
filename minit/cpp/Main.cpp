efe3326194030a921ade6dccb2313738ae9ca834
non258 <c0117037bf@edu.teu.ac.jp>
2018-10-22 20:24:16 +0900

    minit

--- minit/cpp/Main.cpp
+++ minit/cpp/Main.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+
+#define rep(i, n, s) for(int (i) = 0; (i) < (n); (i) += (s))
+
 using namespace std;
+
+template <class T>
+void printv(vector<T> v)
+{
+  cout << '[';
+  for (auto vec: v)
+  {
+    cout << vec << ' ';
+  }
+  cout << ']' << endl;
+}
 
 int main()
 {
