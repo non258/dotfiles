a196dc98ef7a310881c35a477dc018b2ac0b7b01
non258 <c0117037bf@edu.teu.ac.jp>
2019-02-24 15:58:09 +0900

    link.sh add nvim i3

--- link.sh
+++ link.sh
@@ -10,3 +10,5 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.conky ~/.conky
+ln -sf ~/dotfiles/i3 ~/.config/i3
+ln -sf ~/dotfiles/nvim ~/.config/nvim
